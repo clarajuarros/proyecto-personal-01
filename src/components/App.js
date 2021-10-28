0a47985c658194b9ae1f6dec402d83e561622c5d
clarajuarros <clarajuarross@gmail.com>
2021-10-28 10:05:26 +0200

    add components

---
+++ src/components/App.js
@@ -0,0 +1,6 @@
+import React, {useEffect, useState} from 'react';
+import { Link, Route } from 'react-router-dom';
+import "../stylesheets/App.scss";
+
+import header from "./header";
+import main from "./main"
